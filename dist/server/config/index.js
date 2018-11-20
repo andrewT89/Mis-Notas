d3b04387289407103b5fbf90020b88d71c56004a
SoftpymesAndres <produccion3@softpymes.com.co>
2018-11-20 12:30:46 -0500

    primer commit

--- dist/server/config/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var secret = exports.secret = '@MiClaveSecreta';
-var mongoDB = exports.mongoDB = process.env.mongoDB || 'mongodb://localhost:27017/platzi-overflow';
-var port = exports.port = process.env.PORT || 3000;
